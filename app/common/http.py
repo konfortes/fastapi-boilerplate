2489916055c0bba0cd3dd1aaa3b5d4f31c82de54
konfortes <konfortes@gmail.com>
2021-11-01 10:48:13 +0200

    disable auth

--- app/common/http.py
+++ app/common/http.py
@@ -2,8 +2,9 @@ from fastapi import Header, HTTPException
 
 
 async def get_token_header(x_token: str = Header(...)):
-    if x_token != "fake-super-secret-token":
-        raise HTTPException(status_code=400, detail="X-Token header invalid")
+    return True
+    # if x_token != "fake-super-secret-token":
+    #     raise HTTPException(status_code=400, detail="X-Token header invalid")
 
 
 async def get_query_token(token: str):
